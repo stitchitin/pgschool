c98edd9e40ac07c060e18ea4984bd9a5f11c3128
marybngozi <maryblessing.umeh@outlook.com>
2019-07-09 11:08:12 +0100

    changed code location

--- application/choice.php
+++ application/choice.php
@@ -7,102 +7,96 @@
       </div>
     </div>
     <div class="row justify-content-center">
-    <div class="col-md-10">
-    <div class="row">
-    <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
-    <div class="block-18 text-center">
-    <div class="text">
-
-  </div>
-  </div>
-  </div>
-  <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
-  <div class="block-18 text-center">
-  <div class="text">
-
-  </div>
-  </div>
-  </div>
-  <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
-  <div class="block-18 text-center">
-  <div class="text">
-
-  </div>
-  </div>
-  </div>
-  <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
-  <div class="block-18 text-center">
-  <div class="text">
-
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text"></div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text"></div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text"></div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 <section class="ftco-section ftco-no-pt ftc-no-pb">
-<div class="container">
-<div class="row">
-<div class="col-lg-6 py-5">
-<img src="../images/undraw_podcast_q6p7.svg" class="img-fluid" alt="">
-<div class="heading-section ftco-animate mt-5">
-<h2 class="mb-4">APPLICATION FORMS</h2>
-<p>Select the form you want to apply with</p>
-</div>
-</div>
-<div class="col-lg-6 py-5">
-<div class="row">
-<div class="col-md-6 ftco-animate">
-<div class="media block-6 services border text-center">
-<div class="icon d-flex align-items-center justify-content-center">
-<span class="flaticon-cloud-computing"></span>
-</div>
-<a href="002/" style="color:#000;">
-  <div class="mt-3 media-body media-body-2">
-  <h3 class="heading">SPGS Form 002</h3>
-  <p>SPGS Form 002-Postgraduate Application Form click to  fill form</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 py-5">
+        <img src="../images/undraw_podcast_q6p7.svg" class="img-fluid" alt="">
+        <div class="heading-section ftco-animate mt-5">
+          <h2 class="mb-4">APPLICATION FORMS</h2>
+          <p>Select the form you want to apply with</p>
+        </div>
+      </div>
+      <div class="col-lg-6 py-5">
+        <div class="row">
+          <div class="col-md-6 ftco-animate">
+            <div class="media block-6 services border text-center">
+              <div class="icon d-flex align-items-center justify-content-center">
+                <span class="flaticon-cloud-computing"></span>
+              </div>
+              <a href="002/" style="color:#000;">
+                <div class="mt-3 media-body media-body-2">
+                  <h3 class="heading">SPGS Form 002</h3>
+                  <p>SPGS Form 002-Postgraduate Application Form click to  fill form</p>
+                </div>
+              </a>
+            </div>
+          </div>
+
+      <div class="col-md-6 ftco-animate">
+    <div class="media block-6 services border text-center">
+    <div class="icon d-flex align-items-center justify-content-center">
+    <span class="flaticon-cloud"></span>
+    </div>
+    <a href="008/" style="color:#000;"><div class="mt-3 media-body media-body-2">
+    <h3 class="heading">SPGS Form 008</h3>
+    <p>SPGS Form 008-Registration of Courses (Subsequent Years) click to download or fill form</p>
+    </div>
+    </div>
+    </div></a>
+    <div class="col-md-6 ftco-animate">
+    <div class="media block-6 services border text-center">
+    <div class="icon d-flex align-items-center justify-content-center">
+    <span class="flaticon-server"></span>
+    </div>
+    <div class="mt-3 media-body media-body-2">
+    <h3 class="heading">VPS</h3>
+    <p>Even the all-powerful Pointing has no control about the blind texts</p>
+    </div>
+    </div>
+    </div>
+    <div class="col-md-6 ftco-animate">
+    <div class="media block-6 services border text-center">
+    <div class="icon d-flex align-items-center justify-content-center">
+    <span class="flaticon-database"></span>
+    </div>
+    <div class="mt-3 media-body media-body-2">
+    <h3 class="heading">Dedicated</h3>
+    <p>Even the all-powerful Pointing has no control about the blind texts</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
   </div>
-</div>
-</div></a>
-<div class="col-md-6 ftco-animate">
-<div class="media block-6 services border text-center">
-<div class="icon d-flex align-items-center justify-content-center">
-<span class="flaticon-cloud"></span>
-</div>
-<a href="008/" style="color:#000;"><div class="mt-3 media-body media-body-2">
-<h3 class="heading">SPGS Form 008</h3>
-<p>SPGS Form 008-Registration of Courses (Subsequent Years) click to download or fill form</p>
-</div>
-</div>
-</div></a>
-<div class="col-md-6 ftco-animate">
-<div class="media block-6 services border text-center">
-<div class="icon d-flex align-items-center justify-content-center">
-<span class="flaticon-server"></span>
-</div>
-<div class="mt-3 media-body media-body-2">
-<h3 class="heading">VPS</h3>
-<p>Even the all-powerful Pointing has no control about the blind texts</p>
-</div>
-</div>
-</div>
-<div class="col-md-6 ftco-animate">
-<div class="media block-6 services border text-center">
-<div class="icon d-flex align-items-center justify-content-center">
-<span class="flaticon-database"></span>
-</div>
-<div class="mt-3 media-body media-body-2">
-<h3 class="heading">Dedicated</h3>
-<p>Even the all-powerful Pointing has no control about the blind texts</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 </section>
 
 <?php include_once "../include/footer.php" ;?>
